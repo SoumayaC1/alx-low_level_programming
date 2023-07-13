e45be63c255b32945969103e8c11bfd006346b83
Soumaya Chaibi <soumaya.chaibi57@gmail.com>
2023-07-13 14:21:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 /**
- * main -printing without printf and puts
- * 
- * Return: always (1)
- *
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
