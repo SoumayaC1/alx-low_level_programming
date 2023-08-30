cd78ad870dff549430763fdfe73c18da0122a93e
Soumaya Chaibi <soumaya.chaibi57@gmail.com>
2023-08-30 13:38:49 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,5 @@
 #include "lists.h"
-
 /**
-<<<<<<< HEAD
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
@@ -9,33 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
-
-	while (h)
-	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
-	}
-
-	return (num);
-=======
- *  * print_listint - prints all the elements of a linked list
- *   * @h: linked list of type listint_t to print
- *    *
- *     * Return: number of nodes
- *      */
-size_t print_listint(const listint_t *h)
-{
-		size_t num = 0;
-
-			while (h)
-					{
-								printf("%d\n", h->n);
-										num++;
-												h = h->next;
-													}
-
-				return (num);
->>>>>>> 2e04aaad926fc7c87481d8797e7fd428ddca2a8f
+ size_t num = 0;
+ while (h)
+ {
+ printf("%d\n", h->n);
+ num++;
+ h = h->next;
+ }
+ return (num);
 }
+
